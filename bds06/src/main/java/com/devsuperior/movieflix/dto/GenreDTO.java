81ad9c8123984e52c90421f5bfb3a6d6a7982bac
Rafael Santos <rafa.05125@gmail.com>
2023-02-21 11:19:19 -0300

    Initial  entities, controllers, dto and repositories

---
+++ bds06/src/main/java/com/devsuperior/movieflix/dto/GenreDTO.java
@@ -0,0 +1,50 @@
+package com.devsuperior.movieflix.dto;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.entities.Movie;
+
+public class GenreDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	
+	private Set<Movie> movies = new HashSet<>();
+	
+	public GenreDTO() {}
+	
+	public GenreDTO(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public GenreDTO(Genre entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Movie> getMovies() {
+		return movies;
+	}
+}
