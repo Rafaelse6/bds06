f946aea562b3a8b1f51f34edc71dda69f5c20eb1
Rafael Santos <rafa.05125@gmail.com>
2023-02-21 11:31:21 -0300

    Movie genre tests approved

--- bds06/src/main/java/com/devsuperior/movieflix/entities/repositories/GenreRepository.java
+++ bds06/src/main/java/com/devsuperior/movieflix/entities/repositories/GenreRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.devsuperior.movieflix.entities.Genre;
 
 public interface GenreRepository extends JpaRepository<Genre, Long>{
-
+	
+	
 }
